96521320a295840158edf57e032a53338b084050
Maja <xiastald@hotmail.com>
2019-12-05 15:13:33 +0100

    minside

--- minside.php
+++ minside.php
@@ -38,29 +38,81 @@ if (!isset($_SESSION)) session_start();
       </ul><br>
     
     </div>
+    </div>
 
-        
-    <div class="row mineaftaler text-center">
-      <div class="col-sm-3">
+    
+<div class="row mineaftaler text-center">
+      <div class="col-sm-4">
         <h2>Kontrakter klar til dig</h2>
-        
-    </div>
-        <div class="col-sm-3">
+        <!-- Container (Pricing Section) -->
+<div class="container-fluid">
+  <div class="row">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h3>Basic</h3>
+        </div>
+        <div class="panel-body">
+          <p><strong>20</strong> Lorem</p>
+          <p><strong>15</strong> Ipsum</p>
+          <p><strong>5</strong> Dolor</p>
+          <p><strong>2</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div class="panel-footer">
+          <h3>$19</h3>
+          <h4>per month</h4>
+          <button class="btn btn-lg">Vis kontrakt</button>
+        </div>
+      </div>      
+    </div>     
+
+    <div class="col-sm-4">
         <h2>Kontrakter du har oprettet</h2>
-        
-    </div>
-        <div class="col-sm-3">
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Pro</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong>50</strong> Lorem</p>
+          <p><strong>25</strong> Ipsum</p>
+          <p><strong>10</strong> Dolor</p>
+          <p><strong>5</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div class="panel-footer">
+          <h3>$29</h3>
+          <h4>per month</h4>
+          <button class="btn btn-lg">Vis kontrakt</button>
+        </div>
+      </div>      
+    </div>        
+          
+        <div class="col-sm-4">
         <h2>Historik</h2>
+      <div class="panel panel-default text-center">
+        <div class="panel-heading">
+          <h1>Premium</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong>100</strong> Lorem</p>
+          <p><strong>50</strong> Ipsum</p>
+          <p><strong>25</strong> Dolor</p>
+          <p><strong>10</strong> Sit</p>
+          <p><strong>Endless</strong> Amet</p>
+        </div>
+        <div class="panel-footer">
+          <h3>$49</h3>
+          <h4>per month</h4>
+          <button class="btn btn-lg">Vis kontrakt</button>
+        </div>
+      </div>      
+    </div>    
+  </div>
+</div>
         
-    </div>
-        <div class="col-sm-3">
-    <p>rtgiorjgr</p>
-        
-    </div>
-        
-    </div>
     </div>
 </div>
+
 <?php
 require_once("includes/footer.php");
 ?>
