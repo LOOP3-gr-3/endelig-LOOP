56c380c967add77bb46e2f73d744e9cf622ddc4a
Maja <xiastald@hotmail.com>
2019-12-07 10:48:30 +0100

    forside

--- index.php
+++ index.php
@@ -4,23 +4,18 @@ require_once("includes/header.php");
 ?>
 
 <style>
-h1 {
-    text-align: center;
-    color:darkblue;
-    }   
-.blaabaggrundforside {
-        background-color: whitesmoke;
-    }
-
-#logopaaforsiden {
-        max-width: 45%;
-        height: auto;
+#kuntilforside {
+    background-image: url(images/perry-grone-lbLgFFlADrY-unsplash.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
     }
 </style>
 
 <!--link til billede på forsiden https://unsplash.com/s/photos/teamwork--> 
+<body id="kuntilforside">
 
-<hr><h1>VELKOMMEN TIL <img src="images/logoudenbaggrund.png" id="logopaaforsiden"></h1><hr>
+</body>
+
 <?php
 require_once("includes/footer.php");
 ?>
