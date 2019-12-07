716af9f690b0ab128972253b95abacfd277e821e
Maja <xiastald@hotmail.com>
2019-12-07 08:56:16 +0100

    gem

---
+++ nemid.php
@@ -0,0 +1,22 @@
+<?php
+$page = ('NemID underskriv');
+require_once("includes/header.php");
+?>
+<style>
+#nemIDfake {
+    background-image: url(images/nemID1.PNG);
+    background-color: whitesmoke;
+    background-repeat: no-repeat;
+    background-size:auto; 
+    }
+</style>
+
+<body id="nemIDfake">   
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>    
+</body>
+</body>
+</html>
+
+<?php
+require_once("includes/footer.php");
+?>
