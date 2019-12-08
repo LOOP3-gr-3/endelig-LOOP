7de28805b83b85d90d40f8f07621803966c0f28c
Maja <xiastald@hotmail.com>
2019-12-08 14:48:00 +0100

    grids for alle størrelsen på min side, ., sm, md, lg, xl

--- minside.php
+++ minside.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION)) session_start();
 </style>
 <div class="container-fluid">
     <div class="row majaminprofil">
-        <div class="col-xs-12 col-sm-12 col-lg-4 minprofilpaaminside">
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 minprofilpaaminside">
             <hr>
             <h1>MIN PROFIL</h1>
             <hr>
@@ -32,12 +32,12 @@ if (!isset($_SESSION)) session_start();
             <br><br><br><br><br><br><br><br><br><br>
         </div>
         <!-- Mine aftaler sektionen -->
-        <div class="col-xs-12 col-sm-12 col-lg-8">
+        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
             <hr>
             <h1>DINE AFTALER</h1>
             <hr>
         </div>
-        <div class="col-xs-12 col-sm-12 col col-lg-4 majakontraktertildig">
+        <div class="col-xs-12 col-sm-12 col-md-4 col col-lg-4 col-xl-4 majakontraktertildig">
             <h2>Kontrakt anmodninger</h2>
             <div class="panel panel-default text-center">
                 <div class="panel-heading">
@@ -49,12 +49,12 @@ if (!isset($_SESSION)) session_start();
                     <p><strong>Dato</strong> Dolor</p>
                 </div>
                 <div class="panel-footer">
-                    <button class="btn btn-lg">Vis kontrakt</button>
+                    <button class="btn btn-warning btn-lg">Vis kontrakt</button>
                 </div>
             </div>
         </div>
 
-        <div class="col-xs-12 col-sm-12 col-lg-4 majaoprettet">
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-lx-4 majaoprettet">
             <h2>Dine kontrakter</h2>
             <div class="panel panel-default text-center">
                 <div class="panel-heading">
@@ -65,20 +65,20 @@ if (!isset($_SESSION)) session_start();
                     <p><strong>Rente</strong>
                 </div>
                 <div class="panel-footer">
-                    <button class="btn btn-lg">Vis kontrakt</button>
+                    <button class="btn btn-warning btn-lg">Vis kontrakt</button>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="row majahistorikaftaler">
-        <div class="col-xs-12 col-sm-12 text-center">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
             <hr>
             <h1>HISTORIK</h1>
             <hr>
         </div>
 
-        <div class="col-xs-12 col-sm-12 col-lg-6">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
             <div class="panel panel-default text-center">
                 <div class="panel-heading">
                     <h3>Dato</h3>
@@ -87,11 +87,11 @@ if (!isset($_SESSION)) session_start();
                     <p><strong>Afsender:</strong>Lorem <strong>Beløb:</strong> Ipsum <strong>Rente:</strong> Sit</p>
                 </div>
                 <div class="panel-footer">
-                    <button class="btn btn-lg">Vis kontrakt</button>
+                    <button class="btn btn-warning btn-lg">Vis kontrakt</button>
                 </div>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-lg-6">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
             <div class="panel panel-default text-center">
                 <div class="panel-heading">
                     <h3>Dato</h3>
@@ -100,7 +100,7 @@ if (!isset($_SESSION)) session_start();
                     <p><strong>Afsender:</strong>Lorem <strong>Beløb:</strong> Ipsum <strong>Rente:</strong> Sit</p>
                 </div>
                 <div class="panel-footer">
-                    <button class="btn btn-lg">Vis kontrakt</button>
+                    <button class="btn btn-warning btn-lg">Vis kontrakt</button>
                 </div>
             </div>
         </div>
