8b5c6229dc0e0578607d96c25ee034561f9f7314
Mette <metteweyergang@gmail.com>
2019-12-09 11:16:41 +0100

    sendt mail

---
+++ sendtmail.php
@@ -0,0 +1,8 @@
+<?php 
+require_once('includes/header.php');
+?>
+<script>alert("Din mail er nu sendt. Vi vil besvare den hurtigst muligt"); window.location.href="raadgivning.php"</script>
+
+<?php 
+require_once('includes/footer.php');
+?>
