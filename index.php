91ac09ae827acead711db96f124be36c9c9711c5
WasfiaHashemi <57392357+WasfiaHashemi@users.noreply.github.com>
2019-12-09 23:07:04 +0100

    index

--- index.php
+++ index.php
@@ -42,7 +42,8 @@ require_once("includes/header.php");
   <img src="images/benjamin-sow-CB4z0uTFSYg-unsplash.jpg" class="img-responsive img-circle margin" style="display:inline" width="350" height="350">
     <h3 class="margin"><strong>VÆK MED ALT DET BESVÆRLIGE</strong></h3>
   <p>Med Mutuum kan du investere eller låne fra bare 100 kroner. Det er dine penge, så du bestemmer hvad og hvordan de skal bruges. Med Mutuum kan du løbende se gode bud og dermed tilpasse <br> den til dine ønsker og behov. Mutuum passer på dine penge, så de er i sikre hænder. </p>
-  <button href="#" class="btn btn-warning btn-lg">OPRET BRUGER</button>
+  <button href="#" class="btn btn-warning btn-block"> <strong>OPRET BRUGER</strong></button>
+    <br>
 </div>
 <br>
 <div class="container wasbokserne"> 
