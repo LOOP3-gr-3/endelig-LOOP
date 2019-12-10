a0082807104bd389379990721a47d2b6359e57ae
Mette <metteweyergang@gmail.com>
2019-12-10 11:50:43 +0100

    KONTRAKT KAN UPLOADE

--- OpretKontrakt.php
+++ OpretKontrakt.php
@@ -2,53 +2,7 @@
     $page = ('Oprettelse af kontrakt');
     require_once('includes/header.php');
     if(!isset($_SESSION)){session_start();}
- 
-    $query1 = "SELECT * FROM beloeb ORDER BY beloeb";    
-    $result1 = mysqli_query($con, $query1);
-
-
-    $query2 = "SELECT * FROM rente ORDER BY rente";    
-    $result2 = mysqli_query($con, $query2);
-
-
-    $query3 = "SELECT * FROM bindingsperiode ORDER BY loebetid";    
-    $result3 = mysqli_query($con, $query3);
-
-
-    $query4 = "SELECT * FROM kontraktbrud ORDER BY brud";    
-    $result4 = mysqli_query($con, $query4);
-
-
-    if(isset($_POST['laantageremail'])) {
-        $mail = $_POST['laantageremail'];
-        $query5 = "SELECT user_id FROM users WHERE mail = '$mail'";
-        $result5 = mysqli_query($con, $query5);
-        $row5 = mysqli_num_rows($result5);
-        $mail_laantager_id = $row5['user_id'];
-    }
-         $beloeb_id_upload = '';
-         $rente_id_upload = '';
-         $bindingsperiode_id_upload = '' ;
-         $kontraktbrud_id_upload = '';
-         $mail_laantager_id_upload = '';
-         $mail_laangiver_id_upload = '';
-
-     if (isset($_POST['dropdownbeloeb']) && isset($_POST['dropdownrente']) && isset($_POST['dropdownloebetid']) && isset($_POST['dropdownkontraktbrud']) && isset($_POST['laantageremail'])) {
-         $beloeb_id_upload = $_POST['dropdownbeloeb'];
-         $rente_id_upload = $_POST['dropdownrente'];
-         $bindingsperiode_id_upload = $_POST['dropdownloebetid'];
-         $kontraktbrud_id_upload = $_POST['dropdownkontraktbrud'];
-         $mail_laantager_id_upload = $_POST['laantageremail'];
-         $mail_laangiver_id_upload = $_POST[$_SESSION['user_id']];
-         
-         if (isset($_POST['submit'])){
-             $upload = "INSERT INTO kontrakt(beloeb_id, rente_id, bindingsperiode_id, kontraktbrud_id, laantager_user_id, laangiver_user_id) VALUES('$beloeb_id_upload,  $rente_id_upload, $bindingsperiode_id_upload, $kontraktbrud_id_upload, $mail_laantager_id_upload, $mail_laangiver_id_upload')";
-             $result6 = mysqli_query($con, $upload);
-             if(!$result6) die(mysqli_error($con));
-         }}
-print_r($_POST);
 ?>
-
 <div class="container-fluid">
     <div>
         <hr>
@@ -59,13 +13,20 @@ print_r($_POST);
         <p><I>Udfyld venligst nedenstående felter til din kontrakt:</I></p><br>
 
         <div class="container">
-            <form method="post">
+            <form method="post" action="opretkontraktaction.php" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
                 <fieldset>
+                    <div name="userid">
+                        <?php 
+                        $user_id;
+                        ?>
+                    </div>
                     <div>
                         <p>Beløb</p>
                         <select class="btn btn-light dropdown-toggle" id="dropdownbeloeb" data-toggle="dropdown" name="dropdownbeloeb">
                             <option selected value="">Vælg et beløb i DKK</option>
                             <?php
+                                $query1 = "SELECT * FROM beloeb ORDER BY beloeb";    
+                                $result1 = mysqli_query($con, $query1);
                                 $row1 = mysqli_num_rows($result1);                          
                                 while($row1 = mysqli_fetch_assoc($result1)) 
                                 {
@@ -88,7 +49,8 @@ print_r($_POST);
                         <select class="btn btn-light dropdown-toggle" id="dropdownrente" data-toggle="dropdown" name="dropdownrente">
                             <option selected value="">Vælg en rente i %</option>
                             <?php 
-                            
+                                $query2 = "SELECT * FROM rente ORDER BY rente";    
+                                $result2 = mysqli_query($con, $query2);
                                 $row2 = mysqli_num_rows($result2);                          
                                 while($row2 = mysqli_fetch_assoc($result2)) 
                                 {
@@ -109,9 +71,9 @@ print_r($_POST);
                         <p>Løbetid</p>
                         <select class="btn btn-light dropdown-toggle" id="dropdownloebetid" data-toggle="dropdown" name="dropdownloebetid">
                             <option selected value="">Vælg løbetiden på kontrakten</option>
-
                             <?php 
-                            
+                            $query3 = "SELECT * FROM bindingsperiode ORDER BY loebetid"; 
+                            $result3 = mysqli_query($con, $query3);
                             $row3 = mysqli_num_rows($result3);                          
                             while($row3 = mysqli_fetch_assoc($result3)) 
                             {
@@ -142,7 +104,8 @@ print_r($_POST);
                         <select class="btn btn-light dropdown-toggle" id="dropdownkontraktbrud" data-toggle="dropdown" name="dropdownkontraktbrud">
                             <option selected value="">Vælg konsekvens ved brud på kontrakt</option>
                             <?php 
-                            
+                            $query4 = "SELECT * FROM kontraktbrud ORDER BY brud";    
+                            $result4 = mysqli_query($con, $query4);
                             $row4 = mysqli_num_rows($result4);                          
                             while($row4 = mysqli_fetch_assoc($result4)) 
                             {
@@ -175,15 +138,16 @@ print_r($_POST);
 
                     <!-- HUSK AT HER SKAL VI SKIFTE URL ADRESSEN UD MED DEN RIGTIGE I STEDET FOR LOCALHOST href="http://localhost/mutuum/mutuum/endelig-LOOP/viskontrakt.php"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! /-->
 
-                    <button role="button" name="submit" type="submit" class="btn btn-primary">Underskriv og gem</button>
-                    <?php echo $beloeb_id_upload . $rente_id_upload . $bindingsperiode_id_upload . $kontraktbrud_id_upload . $mail_laantager_id_upload . $mail_laangiver_id_upload . $mail_laangiver_id_upload ?>
+                    <a role="button" name="submit" type="submit" class="btn btn-primary blaaknap">Underskriv og gem</a>
+                    
                 </fieldset>
             </form>
+            
             <br>
         </div>
 
     </div>
 </div>
-<?php 
+<?php
  require_once('includes/footer.php');
 ?>
